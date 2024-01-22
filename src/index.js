107a6e5d11c3370e8e834e0e1f86e49570574470
rmh96 <100729581+rmh96@users.noreply.github.com>
2024-01-22 18:10:46 +0530

    Added star rating component with logics & css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,61 @@
 import React from "react";
+import "./index.css";
 
-const StarRating = () => {
-  return <h1>Star Rating Component</h1>;
+const StarRating = ({
+  noOfStars,
+  starFillColor,
+  starBorderColor,
+  timeOutToFade,
+  resetFilledStar,
+}) => {
+  const [hoverStar, setHoverStar] = useState([]);
+  const [clickedStar, setClickedStar] = useState(-1);
+  const onHoverStar = (index) => {
+    const iterates = Array.from({ length: index + 1 }, (_, index) => index);
+    setHoverStar(iterates);
+  };
+  const onUnHoverStar = (index) => {
+    setHoverStar([]);
+  };
+  const onStarClick = (index) => {
+    setClickedStar(index);
+  };
+
+  useEffect(() => {
+    timeOutToFade &&
+      setTimeout(() => {
+        setClickedStar(-1);
+      }, 2000);
+  }, [clickedStar]);
+
+  useEffect(() => {
+    setClickedStar(-1);
+  }, [resetFilledStar]);
+  console.log("hover", hoverStar);
+  return (
+    <div className="sr-con">
+      {Array.from({ length: noOfStars }, (_, index) => (
+        <div
+          className="star"
+          key={index}
+          onMouseEnter={() => onHoverStar(index)}
+          onMouseLeave={() => onUnHoverStar(index)}
+          onClick={() => onStarClick(index)}
+        >
+          <Icon
+            fillColor={
+              hoverStar.includes(index)
+                ? starFillColor
+                : index <= clickedStar
+                ? starFillColor
+                : "white"
+            }
+            borderColor={starBorderColor}
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default StarRating;
